4fd5b1ebfd0b03c6c53e46114c0b7355c53f7dc1
Aachal131 <83298583+Aachal131@users.noreply.github.com>
2024-02-02 22:56:00 +0530

    Add files via upload

--- MaximumProblem.java
+++ MaximumProblem.java
@@ -3,19 +3,6 @@ package com.pratic;
 import java.util.Scanner;
 
 public class MaximumProblem {
-    public static Integer findMax(Integer a, Integer b, Integer c) {
-        Integer max = a;
-
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-
-        return max;
-    }
     public static Float findMax(Float a, Float b, Float c) {
         Float max = a;
 
@@ -31,12 +18,6 @@ public class MaximumProblem {
     }
 
     public static void main (String[] args){
-        Integer maxInt1 = findMax(10, 5, 7);
-        System.out.println("Test Case 1 - Max: " + maxInt1);
-        Integer maxInt2 = findMax(3, 8, 4);
-        System.out.println("Test Case 2 - Max: " + maxInt2);
-        Integer maxInt3 = findMax(2, 6, 9);
-        System.out.println("Test Case 3 - Max: " + maxInt3);
         Float maxFloat1 = findMax(10.5f, 5.2f, 7.8f);
         System.out.println("Test Case 1 - Max: " + maxFloat1);
         Float maxFloat2 = findMax(3.7f, 8.1f, 4.3f);
